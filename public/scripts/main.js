375406977bd5278d768dd28ffc4893ff3c746b40
Shen <shenj7@rose-hulman.edu>
2021-11-10 20:46:17 -0500

    node running on firebase, project restructure

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,3 @@
-const $ = require("jquery");
-
 var rhit = rhit || {};
 
 var spotify = spotify || {};
@@ -10,6 +8,8 @@ spotify.client_id = '0e95f084d6ea4406b2ac1dae208e4a6a'; // Your client id
 // spotify.client_secret = 'd3ea2e92a73a40cabf7698511fc9c9ee'; // Your secret
 spotify.redirect_uri = 'https://csse-280-loaf-of-breadify.web.app/pages/home.html'; // Your redirect uri
 
+apiurl = "http://localhost:5001/csse-280-loaf/us-central1/api/"
+
 /* end spotify api stuff */
 
 
@@ -18,26 +18,35 @@ rhit.mainPageController = null;
 rhit.MainPageController = class {
 	constructor() {
 		this.initializeButtons();
+		const queryString = window.location.search;
+		const urlParams = new URLSearchParams(queryString);
+		this.accessToken = urlParams.get("access_token");
 	}
 
 	initializeButtons() {
 		$("#spotifyButton").click((event) => {
-			window.location.href="/login";
+			window.location.href = apiurl + "login";
+			// fetch(apiurl + "callback").then(() =>{
+			// 	accessToken = new URLSearchParams(url.search).get('access_token');
+			// 	window.location.href = 'http://localhost:5000/pages/home.html';
+			// 	console.log(accessToken);
+			// }
+			// );
 		});
 
 		$("#homeButton").click((event) => {
 			console.log("home");
-			window.location.href = "../pages/home.html";
+			window.location.href = "/pages/home.html";
 		});
 
 		$("#settingsButton").click((event) => {
 			console.log("settings");
-			window.location.href = "../pages/settings.html";
+			window.location.href = "/pages/settings.html";
 
 		});
 		$("#uploadButton").click((event) => {
 			console.log("upload");
-			window.location.href = "../pages/upload.html";
+			window.location.href = "/pages/upload.html";
 
 		});
 		$("#fullscreenButton").click((event) => {
